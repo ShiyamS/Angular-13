919415e3432c0533005a72aec24f13e7a8c74595
Shiyam <shiyams315@gmail.com>
2023-09-21 15:02:32 +0530

    Router - Using Observalble to reterive Paramaters

--- src/app/courses/course/course/course.component.ts
+++ src/app/courses/course/course/course.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
 import { CoursesService } from 'src/app/services/courses.service';
 
 @Component({
@@ -7,19 +8,41 @@ import { CoursesService } from 'src/app/services/courses.service';
   templateUrl: './course.component.html',
   styleUrls: ['./course.component.css']
 })
-export class CourseComponent {
+export class CourseComponent implements OnInit, OnDestroy {
   constructor(private service: CoursesService, private route: ActivatedRoute) { }
 
   course: any;
   courseId: any;
+  private readonly destory = new Subject<boolean>();
 
   ngOnInit(): void {
+    // Use Snapshot when u think they wont by any change in any course of time for the Param ID
+
+
     // This is the better way to get the ID
-    this.courseId = this.route.snapshot.paramMap.get('id');
+    // this.courseId = this.route.snapshot.paramMap.get('id');
 
     // This is the old approach
     // this.courseId = this.route.snapshot.params['id'];
     //this.courseId = this.route.snapshot.params['name'];
-    this.course = this.service.courses.find(x => x.id == this.courseId);
+    // this.course = this.service.courses.find(x => x.id == this.courseId);
+
+
+
+    //  If the Param ID will be chnaging during the course of time try to use Param and subscribe
+
+    this.route.paramMap
+      .pipe(takeUntil(this.destory))
+      .subscribe((param) => {
+        this.courseId = param.get('id');
+        this.course = this.service.courses.find((x) => x.id == this.courseId)
+      })
+  }
+
+
+  ngOnDestroy(): void {
+    console.log("course comp destroyed")
+    this.destory.next(true);
+    this.destory.complete();
   }
 }
