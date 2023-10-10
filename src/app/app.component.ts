a9ec405888984c5dfa5daca96a6a5cf598a185e8
Shiyam <shiyams315@gmail.com>
2023-10-10 23:08:01 +0530

    Forms = Template Form Base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute, Event, NavigationCancel, NavigationEnd, NavigationError
 import { AuthService } from './services/auth.service';
 import { StudentsService } from './services/students.service';
 import { Student } from './student';
+import { NgForm } from '@angular/forms';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -11,128 +13,135 @@ import { Student } from './student';
 })
 export class AppComponent implements OnInit {
 
-  students!: Student[];
-  filteredStudents!: Student[];
-  totalMarks!: number;
-  title = 'routing';
-  _filterTextValue: string = "";
+  // students!: Student[];
+  // filteredStudents!: Student[];
+  // totalMarks!: number;
+  // title = 'routing';
+  // _filterTextValue: string = "";
 
-  totalStudents = new Promise<number>((resolve, reject) => {
-    setTimeout(() => {
-      resolve(this.filteredStudents.length)
-    }, 2000)
-  })
+  // totalStudents = new Promise<number>((resolve, reject) => {
+  //   setTimeout(() => {
+  //     resolve(this.filteredStudents.length)
+  //   }, 2000)
+  // })
 
-  spinnerDiv = false;
+  // spinnerDiv = false;
 
-  constructor(private activatedRoute: ActivatedRoute, private authUser: AuthService, private router: Router, private StudentService: StudentsService) { }
-
-
-  get filterTextValue() {
-    return this._filterTextValue;
-  }
-
-  set filterTextValue(filteredValue: string) {
-    this._filterTextValue = filteredValue;
-    this.filteredStudents = this.filteredStudentByGender(filteredValue);
-  }
-
-  filteredStudentByGender(filteredValue: string) {
-    if (this.filterTextValue === "" || this.students.length == 0) {
-      return this.students
-    } else {
-      return this.students.filter((student) => {
-        return student.gender.toLowerCase() === filteredValue.toLowerCase();
-      })
-    }
-  }
-
-  ngOnInit(): void {
-
-    this.students = this.StudentService.students;
-    this.totalMarks = this.StudentService.totalMarks;
-
-    this.filteredStudents = this.students;
+  // constructor(private activatedRoute: ActivatedRoute, private authUser: AuthService, private router: Router, private StudentService: StudentsService) { }
 
 
+  // get filterTextValue() {
+  //   return this._filterTextValue;
+  // }
 
-    this.activatedRoute.fragment.subscribe((value) => {
-      // console.log(value)
-      if (value) {
-        this.smoothScroll(value);
-      }
+  // set filterTextValue(filteredValue: string) {
+  //   this._filterTextValue = filteredValue;
+  //   this.filteredStudents = this.filteredStudentByGender(filteredValue);
+  // }
 
-    });
+  // filteredStudentByGender(filteredValue: string) {
+  //   if (this.filterTextValue === "" || this.students.length == 0) {
+  //     return this.students
+  //   } else {
+  //     return this.students.filter((student) => {
+  //       return student.gender.toLowerCase() === filteredValue.toLowerCase();
+  //     })
+  //   }
+  // }
 
-    this.router.events.subscribe((route: Event) => {
-      if (route instanceof NavigationStart) {
-        this.spinnerDiv = true;
-      }
+  // ngOnInit(): void {
 
-      if (route instanceof NavigationEnd || route instanceof NavigationCancel || route instanceof NavigationError) {
-        this.spinnerDiv = false;
-      }
-    })
+  //   this.students = this.StudentService.students;
+  //   this.totalMarks = this.StudentService.totalMarks;
 
-
-  }
-
-  addFakeStudentData() {
-    // let studentCopy = Object.assign([], this.students);
-    // studentCopy.push({
-    //   name: 'Test 9',
-    //   course: 'MCA - Full Stack',
-    //   marks: 345,
-    //   DOB: new Date(),
-    //   gender: 'Male',
-    // })
-    // Once a fake copy is created, and then the value is re assigned in order to reflect the data
-    // If manipulate the reference data, its doses not reflects and become a impure pipe
-    // this.students = studentCopy;
-
-    //Impure Pipe
-    this.students.push({
-      name: 'Test 9',
-      course: 'MCA - Full Stack',
-      marks: 345,
-      DOB: new Date(),
-      gender: 'Male',
-    })
-
-    this.filteredStudents = this.filteredStudentByGender(this._filterTextValue);
-  }
-
-  changeGender() {
-    // This become an inpur change as the main student data in not refelecting the change during filtering.
-    // this.StudentService.students[0].gender = "Female"
-
-    // let studentCopy2: Student[] = Object.assign([], this.students);
-    // studentCopy2[0].gender = 'female';
-    // this.students = studentCopy2;
-
-    //Impure change
-    this.students[0].gender = "Female"
-    this.filteredStudents = this.filteredStudentByGender(this._filterTextValue);
-
-  }
-
-  onMouseMove() {
-
-  }
+  //   this.filteredStudents = this.students;
 
 
 
+  //   this.activatedRoute.fragment.subscribe((value) => {
+  //     // console.log(value)
+  //     if (value) {
+  //       this.smoothScroll(value);
+  //     }
 
-  smoothScroll(id: string) {
-    document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' })
-  }
+  //   });
 
-  logIn() {
-    this.authUser.login();
-  }
+  //   this.router.events.subscribe((route: Event) => {
+  //     if (route instanceof NavigationStart) {
+  //       this.spinnerDiv = true;
+  //     }
 
-  logOut() {
-    this.authUser.logOut();
-    this.router.navigate(['']);
+  //     if (route instanceof NavigationEnd || route instanceof NavigationCancel || route instanceof NavigationError) {
+  //       this.spinnerDiv = false;
+  //     }
+  //   })
+
+
+  // }
+
+  // addFakeStudentData() {
+  //   // let studentCopy = Object.assign([], this.students);
+  //   // studentCopy.push({
+  //   //   name: 'Test 9',
+  //   //   course: 'MCA - Full Stack',
+  //   //   marks: 345,
+  //   //   DOB: new Date(),
+  //   //   gender: 'Male',
+  //   // })
+  //   // Once a fake copy is created, and then the value is re assigned in order to reflect the data
+  //   // If manipulate the reference data, its doses not reflects and become a impure pipe
+  //   // this.students = studentCopy;
+
+  //   //Impure Pipe
+  //   this.students.push({
+  //     name: 'Test 9',
+  //     course: 'MCA - Full Stack',
+  //     marks: 345,
+  //     DOB: new Date(),
+  //     gender: 'Male',
+  //   })
+
+  //   this.filteredStudents = this.filteredStudentByGender(this._filterTextValue);
+  // }
+
+  // changeGender() {
+  //   // This become an inpur change as the main student data in not refelecting the change during filtering.
+  //   // this.StudentService.students[0].gender = "Female"
+
+  //   // let studentCopy2: Student[] = Object.assign([], this.students);
+  //   // studentCopy2[0].gender = 'female';
+  //   // this.students = studentCopy2;
+
+  //   //Impure change
+  //   this.students[0].gender = "Female"
+  //   this.filteredStudents = this.filteredStudentByGender(this._filterTextValue);
+
+  // }
+
+  // onMouseMove() {
+
+  // }
+
+
+
+
+  // smoothScroll(id: string) {
+  //   document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' })
+  // }
+
+  // logIn() {
+  //   this.authUser.login();
+  // }
+
+  // logOut() {
+  //   this.authUser.logOut();
+  //   this.router.navigate(['']);
+  // }
+  // Template Form
+
+  ngOnInit() { }
+
+  onSubmit(form: NgForm) {
+    console.log(form.value)
   }
 }
