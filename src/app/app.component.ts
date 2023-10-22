cb9cc03a275bc9c321f186085995d8506e0b13e1
Shiyam <shiyams315@gmail.com>
2023-10-22 17:14:01 +0530

    Http Request Get - using Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,15 @@ import { StudentsService } from './services/students.service';
 import { Student } from './student';
 import { FormArray, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { NgFor } from '@angular/common';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+interface Product {
+  id?: string;
+  productDesc: string;
+  productName: string
+  productPrice: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -267,23 +274,39 @@ export class AppComponent implements OnInit {
   //   return response;
   // }
   @ViewChild('createProduct') createProduct: NgForm;
-
-  constructor(private http: HttpClient) {
-
-  }
-
+  allProducts: Product[] = [];
+  constructor(private http: HttpClient) { }
   ngOnInit() {
-
+    this.getProducts();
   }
 
+  onProductFetch() {
+    this.getProducts();
+  }
 
   addProduct(data: { productName: string, productDesc: string, productPrice: string }) {
     const headersHttp = new HttpHeaders({ 'myHeaders': 'Manage Products' });
-    this.http.post('https://angularhttprequest-638f4-default-rtdb.firebaseio.com/products.json', data, { headers: headersHttp }).subscribe((res) => {
+    this.http.post<{ name: string }>('https://angularhttprequest-638f4-default-rtdb.firebaseio.com/products.json', data, { headers: headersHttp }).subscribe((res) => {
       console.log(res);
       this.createProduct.reset();
 
     });
 
   }
+  private getProducts() {
+    this.http.get<{ [key: string]: Product }>('https://angularhttprequest-638f4-default-rtdb.firebaseio.com/products.json')
+      .pipe(map((res) => {
+        let products = [];
+        for (const key in res) {
+          products.push({ ...res[key], id: key })
+        }
+        return products;
+      }))
+      .subscribe((products) => {
+        console.log(products);
+        this.allProducts = products;
+        console.log("all Prod", this.allProducts)
+      })
+  }
 }
+
